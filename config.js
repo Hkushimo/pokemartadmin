43ce73d530394c7bf24764a55ecbcb217e8bbd0a
Hakeem Kushimo <hkushimo@gmail.com>
2026-09-20 11:17:34 -0400

    Configure Apps Script backend

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.POKEMART_ADMIN_CONFIG = {
-  scriptUrl: ""
+  scriptUrl: "https://script.google.com/macros/s/AKfycbzMx8YyWjJ4iG4i0OmApObAbaXphhUtco_4_ExQW-kysh74DLw9KQOlkSBTUWX7nzDXEA/exec"
 };
